d3fd7bc721e8f7ae48741095735774d1b759c5e2
Gordon Stewart <gstew5@gmail.com>
2018-01-25 17:32:35 -0500

    Updates

--- AST.hs
+++ AST.hs
@@ -1,4 +1,9 @@
-{-# LANGUAGE GADTs, MultiParamTypeClasses #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 module AST where
 
 import Control.Monad.Trans.State.Lazy
@@ -7,33 +12,67 @@ class Field f where
 
 class Value v where
 
-class (Field f, Value v) => Instruction i f v where
-  { getInstructionField :: i -> f -> v
+class (Field f, Value v) => Record f v t where
+  { getField :: t -> f -> v
   }
 
-class (Field f, Value v) => Result r f v where
-  { getResultField :: r -> f -> v
-  }
+data Binop = Sum | Prod
+
+data Unop = Neg
 
 data Pred fi vi i fr vr r where
   PZero :: Pred fi vi i fr vr r
   POne :: Pred fi vi i fr vr r
-  PTestInstruction :: Instruction i fi vi => i -> fi -> vi -> Pred fi vi i fr vr r
-  PTestResult :: Result r fr vr => r -> fr -> vr -> Pred fi vi i fr vr r
-  PSum :: Pred fi vi i fr vr r -> Pred fi vi i fr vr r -> Pred fi vi i fr vr r
-  PProd :: Pred fi vi i fr vr r -> Pred fi vi i fr vr r -> Pred fi vi i fr vr r
-  PNeg :: Pred fi vi i fr vr r -> Pred fi vi i fr vr r 
+  PTestInstruction :: Record i fi vi =>
+    i -> fi -> vi ->
+    Pred fi vi i fr vr r
+  PTestResult :: Record r fr vr =>
+    r -> fr -> vr ->
+    Pred fi vi i fr vr r
+  PBin ::
+    Binop -> 
+    Pred fi vi i fr vr r ->
+    Pred fi vi i fr vr r ->
+    Pred fi vi i fr vr r
+  PUn ::
+    Unop -> 
+    Pred fi vi i fr vr r ->
+    Pred fi vi i fr vr r
+
+pprod = PBin Prod
+psum = PBin Sum
+pneg = PUn Neg
+
+data Slice = Action | Result
 
 data Policy fi vi i fr vr r where
-  PlTest :: Pred fi vi i fr vr r -> Policy fi vi i fr vr r -> Policy fi vi i fr vr r
-  PlSliceA :: Policy fi vi i fr vr r -> Policy fi vi i fr vr r
-  PlSliceR :: Policy fi vi i fr vr r -> Policy fi vi i fr vr r
-  PlInjA :: Instruction inew finew vinew => inew -> finew -> vinew -> Policy fi vi i fr vr r -> Policy fi vi i fr vr r
-  PlInjR :: Result rnew frnew vrnew => rnew -> frnew -> vrnew -> Policy fi vi i fr vr r -> Policy fi vi i fr vr r
-  PlUpdateInstruction :: Instruction i fi vi => i -> fi -> vi -> Policy fi vi i fr vr r -> Policy fi vi i fr vr r
-  PlUpdateResult :: Result r fr vr => r -> fr -> vr -> Policy fi vi i fr vr r -> Policy fi vi i fr vr r
-  PlChoice :: Policy fi vi i fr vr r -> Policy fi vi i fr vr r -> Policy fi vi i fr vr r
-  PlConcat :: Policy fi vi i fr vr r -> Policy fi vi i fr vr r -> Policy fi vi i fr vr r
+  PlTest ::
+    Pred fi vi i fr vr r ->
+    Policy fi vi i fr vr r ->
+    Policy fi vi i fr vr r
+  PlSlice ::
+    Slice -> 
+    Policy fi vi i fr vr r ->
+    Policy fi vi i fr vr r
+  PlInjA ::
+    i -> 
+    Policy fi vi i fr vr r
+  PlInjR :: 
+    r -> 
+    Policy fi vi i fr vr r
+  PlUpdateInstruction :: Record i fi vi =>
+    fi -> vi ->
+    Policy fi vi i fr vr r ->
+    Policy fi vi i fr vr r
+  PlUpdateResult :: Record r fr vr =>
+    fr -> vr ->
+    Policy fi vi i fr vr r ->
+    Policy fi vi i fr vr r
+  PlBin ::
+    Binop -> 
+    Policy fi vi i fr vr r ->
+    Policy fi vi i fr vr r ->
+    Policy fi vi i fr vr r
   
 class Reg r where
 
@@ -83,7 +122,9 @@ compile_pred iin iout rin rout POne =
      }  
      
 mux :: Buffer -> Buffer -> Buffer -> CompileM Exp
+mux b _ _ = return $ EForever (EWrite b (MkId "BOGUS"))
 
-demux :: Buffer -> Buffer -> Buffer -> CompileM Exp 
+demux :: Buffer -> Buffer -> Buffer -> CompileM Exp
+demux b _ _ = return $ EForever (EWrite b (MkId "BOGUS"))
 
 
