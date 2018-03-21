8ff0c719bfe611b7c085dffd62a3632c525e2df1
16seftos <seftonsg@icloud.com>
2018-03-21 19:19:10 -0400

    Last "correction" was slightly wrong.  Still needs to be in hexadecimal though. SFI.v has passed an extensive test.

--- verilog_tests/SFI/SFI_tb.v
+++ verilog_tests/SFI/SFI_tb.v
@@ -51,9 +51,39 @@ module SFI_tb;
         
     // Add stimulus here
     $monitor("\nTesting");#1;
-    $monitor("\nTestintg bad eff addr:");
+    $monitor("\nTestintg non-store instructions:");
+	   //these are from other programs I wrote, or random inputs
       ri     <= 64'hBAD0ADD012345678;
-      exp_ro <= 64'h0000000000000000;
+      exp_ro <= 64'hBAD0ADD012345678;
+      #1;
+      if(exp_ro == ro) begin
+        $monitor("\tPassed");
+      end else begin
+        $monitor("\tFailed");
+	     all_pass = 0;
+      end
+		ri     <= 64'h0021302000432820;
+      exp_ro <= 64'h0021302000432820;
+		// Should this pass the last 32b?
+      #1;
+      if(exp_ro == ro) begin
+        $monitor("\tPassed");
+      end else begin
+        $monitor("\tFailed");
+	     all_pass = 0;
+      end
+		ri     <= 64'h2021000100031020;
+      exp_ro <= 64'h2021000100031020;
+		// Should this pass the last 32b?
+      #1;
+      if(exp_ro == ro) begin
+        $monitor("\tPassed");
+      end else begin
+        $monitor("\tFailed");
+	     all_pass = 0;
+      end
+		ri     <= 64'h0123456789ABCDEF;
+      exp_ro <= 64'h0123456789ABCDEF;
 		// Should this pass the last 32b?
       #1;
       if(exp_ro == ro) begin
@@ -64,9 +94,101 @@ module SFI_tb;
       end
     #10;
     
-    $monitor("\nTestintg good eff addr:");
-      ri     <= 64'hA219987200000000;
-      exp_ro <= 64'hA219987200000000;
+	 //GOOD TESTS
+    $monitor("\nTestintg good eff addrs:");
+	   //SB
+      ri     <= 64'hFAFA0000A0111111;
+      exp_ro <= 64'hA2FA0000A0111111;
+      #1;
+      if(exp_ro == ro) begin
+        $monitor("\tPassed");
+      end else begin
+        $monitor("\tFailed");
+	     all_pass = 0;
+      end
+		//SC
+      ri     <= 64'hFACA0001E0111111;
+      exp_ro <= 64'hA2CA0001E0111111;
+      #1;
+      if(exp_ro == ro) begin
+        $monitor("\tPassed");
+      end else begin
+        $monitor("\tFailed");
+	     all_pass = 0;
+      end
+		//SCD
+      ri     <= 64'h01000002F0111111;
+      exp_ro <= 64'hA2000002F0111111;
+      #1;
+      if(exp_ro == ro) begin
+        $monitor("\tPassed");
+      end else begin
+        $monitor("\tFailed");
+	     all_pass = 0;
+      end
+		//SD
+      ri     <= 64'h2A321403FC111111;
+      exp_ro <= 64'hA2321403FC111111;
+      #1;
+      if(exp_ro == ro) begin
+        $monitor("\tPassed");
+      end else begin
+        $monitor("\tFailed");
+	     all_pass = 0;
+      end
+		//SDL
+      ri     <= 64'hA1222004B8111111;
+      exp_ro <= 64'hA2222004B8111111;
+      #1;
+      if(exp_ro == ro) begin
+        $monitor("\tPassed");
+      end else begin
+        $monitor("\tFailed");
+	     all_pass = 0;
+      end
+		//SDR
+      ri     <= 64'hB1000005B4111111;
+      exp_ro <= 64'hA2000005B4111111;
+      #1;
+      if(exp_ro == ro) begin
+        $monitor("\tPassed");
+      end else begin
+        $monitor("\tFailed");
+	     all_pass = 0;
+      end
+		//SH
+      ri     <= 64'hF2CAFE06A4111111;
+      exp_ro <= 64'hA2CAFE06A4111111;
+      #1;
+      if(exp_ro == ro) begin
+        $monitor("\tPassed");
+      end else begin
+        $monitor("\tFailed");
+	     all_pass = 0;
+      end
+		//SW
+      ri     <= 64'hFFCAD007AC111111;
+      exp_ro <= 64'hA2CAD007AC111111;
+      #1;
+      if(exp_ro == ro) begin
+        $monitor("\tPassed");
+      end else begin
+        $monitor("\tFailed");
+	     all_pass = 0;
+      end
+		//SWL
+      ri     <= 64'h00000008A8111111;
+      exp_ro <= 64'hA2000008A8111111;
+      #1;
+      if(exp_ro == ro) begin
+        $monitor("\tPassed");
+      end else begin
+        $monitor("\tFailed");
+	     all_pass = 0;
+      end
+		//SWR
+      ri     <= 64'h44FACE09B8111111;
+      exp_ro <= 64'hA2FACE09B8111111;
       #1;
       if(exp_ro == ro) begin
         $monitor("\tPassed");
@@ -75,7 +197,116 @@ module SFI_tb;
 	     all_pass = 0;
       end
     #10;
+	 
+	 
+	 
+	 //Bad Tests:
+	 $monitor("\nTestintg bad eff addrs:");
+      //SB
+      ri     <= 64'hA2000000A0111111;
+      exp_ro <= 64'hA2000000A0111111;
+      #1;
+      if(exp_ro == ro) begin
+        $monitor("\tPassed");
+      end else begin
+        $monitor("\tFailed");
+	     all_pass = 0;
+      end
+		//SC
+      ri     <= 64'hA20A0001E0111111;
+      exp_ro <= 64'hA20A0001E0111111;
+      #1;
+      if(exp_ro == ro) begin
+        $monitor("\tPassed");
+      end else begin
+        $monitor("\tFailed");
+	     all_pass = 0;
+      end
+		//SCD
+      ri     <= 64'hA2000002F0111111;
+      exp_ro <= 64'hA2000002F0111111;
+      #1;
+      if(exp_ro == ro) begin
+        $monitor("\tPassed");
+      end else begin
+        $monitor("\tFailed");
+	     all_pass = 0;
+      end
+		//SD
+      ri     <= 64'hA2321403FC111111;
+      exp_ro <= 64'hA2321403FC111111;
+      #1;
+      if(exp_ro == ro) begin
+        $monitor("\tPassed");
+      end else begin
+        $monitor("\tFailed");
+	     all_pass = 0;
+      end
+		//SDL
+      ri     <= 64'hA2222004B8111111;
+      exp_ro <= 64'hA2222004B8111111;
+      #1;
+      if(exp_ro == ro) begin
+        $monitor("\tPassed");
+      end else begin
+        $monitor("\tFailed");
+	     all_pass = 0;
+      end
+		//SDR
+      ri     <= 64'hA2000005B4111111;
+      exp_ro <= 64'hA2000005B4111111;
+      #1;
+      if(exp_ro == ro) begin
+        $monitor("\tPassed");
+      end else begin
+        $monitor("\tFailed");
+	     all_pass = 0;
+      end
+		//SH
+      ri     <= 64'hA2CAFE06A4111111;
+      exp_ro <= 64'hA2CAFE06A4111111;
+      #1;
+      if(exp_ro == ro) begin
+        $monitor("\tPassed");
+      end else begin
+        $monitor("\tFailed");
+	     all_pass = 0;
+      end
+		//SW
+      ri     <= 64'hA2CAD007AC111111;
+      exp_ro <= 64'hA2CAD007AC111111;
+      #1;
+      if(exp_ro == ro) begin
+        $monitor("\tPassed");
+      end else begin
+        $monitor("\tFailed");
+	     all_pass = 0;
+      end
+		//SWL
+      ri     <= 64'hA2000008A8111111;
+      exp_ro <= 64'hA2000008A8111111;
+      #1;
+      if(exp_ro == ro) begin
+        $monitor("\tPassed");
+      end else begin
+        $monitor("\tFailed");
+	     all_pass = 0;
+      end
+		//SWR
+      ri     <= 64'hA2FACE09B8111111;
+      exp_ro <= 64'hA2FACE09B8111111;
+      #1;
+      if(exp_ro == ro) begin
+        $monitor("\tPassed");
+      end else begin
+        $monitor("\tFailed");
+	     all_pass = 0;
+      end
+    #10;
+    #10;
     
+	 
+	 //FINISHED TESTING
     $monitor("\nDone testing, exiting\n");#1;
 	 if(all_pass == 1) begin
 	   $monitor("All Pass");#1;
